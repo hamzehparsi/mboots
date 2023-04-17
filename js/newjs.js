20910b9e8cf8b24b9e7896a170a82077abd3a5c2
hamzehparsi <hamzehparsi@gmail.com>
2023-04-17 23:33:25 +0430

    first commit hoghoghi

--- js/newjs.js
+++ js/newjs.js
@@ -3,7 +3,7 @@ items.forEach((element) => {
   element.addEventListener("mouseover", function handlehover(event) {
     subMenu = element.children[1];
     subMenu.style.opacity = "100";
-    subMenu.style.top = "50px";
+    subMenu.style.top = "100px";
     subMenu.style.visibility = "visible";
   });
 });
@@ -11,7 +11,7 @@ items.forEach((element) => {
   element.addEventListener("mouseleave", function handlehover(event) {
     subMenu = element.children[1];
     subMenu.style.opacity = "0";
-    subMenu.style.top = "50px";
+    subMenu.style.top = "100px";
     subMenu.style.visibility = "hidden";
   });
 });
@@ -29,83 +29,13 @@ let modalBox = document.querySelector('.modal')
 modalContent.addEventListener('click', () => {
   modalBox.classList.toggle('show')
 })
-// newProducts.addEventListener('mouseover', () => {
-//     subMenu.style.opacity = '100';
-//     subMenu.style.top = '140px';
-//     subMenu.style.visibility = 'visible';
-//     alert('jks')
-// })
-// newProducts.addEventListener('mouseleave', () => {
-//     subMenu.style.opacity = '0';
-//     subMenu.style.top = '165px';
-//     subMenu.style.visibility = 'hidden';
-// })
 
-// let man = document.querySelector('#man')
-// let manMenu = document.querySelector('.man-menu')
-// man.addEventListener('mouseover', () => {
-//     manMenu.style.opacity = '100';
-//     manMenu.style.top = '145px';
-//     manMenu.style.visibility = 'visible';
-// })
-// man.addEventListener('mouseleave', () => {
-//     manMenu.style.opacity = '0';
-//     manMenu.style.top = '165px';
-//     manMenu.style.visibility = 'hidden';
-// })
 
-// let woman = document.querySelector('#woman')
-// let womanMenu = document.querySelector('.woman-menu')
-// woman.addEventListener('mouseover', () => {
-//     womanMenu.style.opacity = '100';
-//     womanMenu.style.top = '145px';
-//     womanMenu.style.visibility = 'visible';
-// })
-// woman.addEventListener('mouseleave', () => {
-//     womanMenu.style.opacity = '0';
-//     womanMenu.style.top = '165px';
-//     womanMenu.style.visibility = 'hidden';
-// })
 
-// let tajhis = document.querySelector('#tajhis')
-// let tajhisMenu = document.querySelector('.tajhis-menu')
-// tajhis.addEventListener('mouseover', () => {
-//     tajhisMenu.style.opacity = '100';
-//     tajhisMenu.style.top = '145px';
-//     tajhisMenu.style.visibility = 'visible';
-// })
-// tajhis.addEventListener('mouseleave', () => {
-//     tajhisMenu.style.opacity = '0';
-//     tajhisMenu.style.top = '165px';
-//     tajhisMenu.style.visibility = 'hidden';
-// })
 
-// let hamrah = document.querySelector('#hamrah')
-// let hamrahMenu = document.querySelector('.hamrah-menu')
-// hamrah.addEventListener('mouseover', () => {
-//     hamrahMenu.style.opacity = '100';
-//     hamrahMenu.style.top = '145px';
-//     hamrahMenu.style.visibility = 'visible';
-// })
-// hamrah.addEventListener('mouseleave', () => {
-//     hamrahMenu.style.opacity = '0';
-//     hamrahMenu.style.top = '165px';
-//     hamrahMenu.style.visibility = 'hidden';
-// })
 
-// let shoes = document.querySelector('#shoes')
-// let shoesMenu = document.querySelector('.shoes-menu')
 
-// shoes.addEventListener('mouseover', () => {
-//     shoesMenu.style.opacity = '100';
-//     shoesMenu.style.top = '145px';
-//     shoesMenu.style.visibility = 'visible';
-// })
-// shoes.addEventListener('mouseleave', () => {
-//     shoesMenu.style.opacity = '0';
-//     shoesMenu.style.top = '165px';
-//     shoesMenu.style.visibility = 'hidden';
-// })
+
 
 $(document).ready(function () {
   $(".owl-one").owlCarousel({
